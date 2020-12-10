cfbbb28f0fbca348869d38b6f8c211966af40976
Lane Garner <b.lane.garner@gmail.com>
2020-12-10 13:16:09 -0600

    cleaned up comments

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,6 @@ const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
 exports.getAllUsers = (req, res) => {
-  // SELECT ALL USERS
   pool.query("SELECT * FROM users JOIN usersAddress ON users.id = usersAddress.user_id JOIN usersContact ON users.id = usersContact.user_id ORDER BY users.id;", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
@@ -11,11 +10,13 @@ exports.getAllUsers = (req, res) => {
 }
 
 exports.getUserById = (req, res) => {
-  // SELECT USERS WHERE ID = <REQ PARAMS ID>
   const id = req.params.id;
 
-  let sql = "SELECT ?? FROM ?? WHERE ?? = ?"
-  // WHAT GOES IN THE BRACKETS
+  let sql = `
+    SELECT ?? 
+    FROM ?? 
+    WHERE ?? = ?`
+
   sql = mysql.format(sql, ['*', "users", "id", id])
 
   pool.query(sql, (err, rows) => {
@@ -23,13 +24,15 @@ exports.getUserById = (req, res) => {
     return res.json(rows);
   })
 }
+
 exports.createUser = (req, res) => {
   let newUser = req.body
   let firstName = newUser.first_name
   let lastName = newUser.last_name
-  // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "INSERT INTO ?? (??, ??) VALUES (?, ?)"
-  // WHAT GOES IN THE BRACKETS
+  let sql = `
+    INSERT INTO ?? (??, ??) 
+    VALUES (?, ?)`
+
   sql = mysql.format(sql, ['users', 'first_name', 'last_name', `${firstName}`, `${lastName}`])
 
   pool.query(sql, (err, results) => {
@@ -39,13 +42,15 @@ exports.createUser = (req, res) => {
 }
 
 exports.updateUserById = (req, res) => {
-  // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
   const id = parseInt(req.params.id)
   const firstName = req.body.first_name
   const lastName = req.body.last_name
 
-  let sql = "UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?"
-  // WHAT GOES IN THE BRACKETS
+  let sql = `
+    UPDATE ?? 
+    SET ?? = ?, ?? = ? 
+    WHERE ?? = ?`
+  
   sql = mysql.format(sql, ['users', 'first_name', firstName, 'last_name', lastName, 'id', id])
 
   pool.query(sql, (err, results) => {
@@ -55,11 +60,13 @@ exports.updateUserById = (req, res) => {
 }
 
 exports.deleteUserByFirstName = (req, res) => {
-  // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
   const firstName = req.params.first_name
 
-  let sql = "DELETE FROM ?? WHERE ?? = ?"
-  // WHAT GOES IN THE BRACKETS
+  let sql = `
+    DELETE 
+    FROM ?? 
+    WHERE ?? = ?`
+    
   sql = mysql.format(sql, ['users', 'first_name', firstName])
 
   pool.query(sql, (err, results) => {
@@ -67,11 +74,3 @@ exports.deleteUserByFirstName = (req, res) => {
     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
   })
 }
-
-// module.exports = {
-//   getAllUsers,
-//   getUserById,
-//   createUser,
-//   updateUserById,
-//   deleteUserByFirstName
-// }
